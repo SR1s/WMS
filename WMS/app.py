853b5b857d2f2c7215445ed8efeed9f7c8409be4
SR1s <seemsnevermind@gmail.com>
2014-03-05 18:32:53 +0800

    add run app script and db build script

---
+++ WMS/app.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+import sys, os
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+from flask import Flask
+from WMS.models import db
+from WMS.models.Income import Income
+
+from WMS.config.database import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, \
+								MYSQL_PASS, MYSQL_DB
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % \
+        								(MYSQL_USER, MYSQL_PASS, MYSQL_HOST, \
+        								 MYSQL_PORT, MYSQL_DB)
+app.debug = True
+app.secret_key = 'guesswhatkeyitis'
+db.init_app(app)
