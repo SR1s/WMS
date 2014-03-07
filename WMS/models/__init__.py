d132d7c0d1f17d4c481d7627a7682d544f6ee07d
SR1s <seemsnevermind@gmail.com>
2014-03-07 16:41:00 +0800

    import new models

--- WMS/models/__init__.py
+++ WMS/models/__init__.py
@@ -2,8 +2,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+from Account import Account
+from Income import Income
+from IncomeDetail import IncomeDetail
 from Item import Item
 from Order import Order
 from OrderDetail import OrderDetail
-from Income import Income
-from IncomeDetail import IncomeDetail
+from Place import Place
