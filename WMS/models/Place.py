fbb203c50d9f09b5403172192e9660c059f904e0
SR1s <seemsnevermind@gmail.com>
2014-03-07 20:24:47 +0800

    add init function for model place

--- WMS/models/Place.py
+++ WMS/models/Place.py
@@ -5,3 +5,9 @@ class Place(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     place = db.Column(db.String(255), unique=True)
     person = db.relationship("Account", backref="place", lazy='dynamic')
+
+    def __init__(self, place):
+        self.place = place
+
+    def __repr__(self):
+        return "<-- Place Object: %s -->" % self.place
