11eb1d2b9647f55dfbc2b0f6ea7ea79e2b6f1039
SR1s <seemsnevermind@gmail.com>
2014-03-08 15:57:02 +0800

    add 404 error page

--- README.md
+++ README.md
@@ -18,10 +18,10 @@ CONFIG
 1. app : WMS/config/app_config.py
 1. database: WMS/config/database.py
 
-RUN
+DEPLOY
 ===
 
 1. fill the database information on config file
 1. run db_build.py script to initial database
 1. run run_test.py script to determine if someting wrong
-1. run run_app.py script to start the project
+1. run run_app.py script to start the application
--- WMS/app.py
+++ WMS/app.py
@@ -3,7 +3,7 @@ import sys, os
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-from flask import Flask
+from flask import Flask, render_template
 from WMS.models import db
 from WMS.models.Income import Income
 from WMS.config import app_config
@@ -15,3 +15,7 @@ db.init_app(app)
 from WMS.controllers.accounts import accounts
 
 app.register_blueprint(accounts, url_prefix="/accounts")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
