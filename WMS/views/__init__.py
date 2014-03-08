099f7152da17513b7d88264b395a970349edad4f
SR1s <seemsnevermind@gmail.com>
2014-03-08 20:11:48 +0800

    add login and verify login and logout logic

--- WMS/views/__init__.py
+++ WMS/views/__init__.py
@@ -1,9 +1,11 @@
+from flask import session, render_template
 import functools
 
 def verify_login(func):
     @functools.wraps(func)
     def wrappper():
-        # test if login
-        func()
-        # do something else
+        if session["status"]:
+            return func()
+        else:
+            return render_template("404.html"), 404
     return wrappper
--- WMS/views/accounts.py
+++ WMS/views/accounts.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, abort, request
+from flask import Blueprint, render_template, abort, request, session
 from WMS.app import db
 from WMS.forms.accounts import LoginForm
 from WMS.models import Place, Account
 from WMS.utils import md5
+from WMS.views import verify_login
 
 accounts = Blueprint("accounts",__name__)
 
@@ -19,11 +20,22 @@ def perform_login():
         user_ps = md5(request.form['user_ps'])
         user = Account.query.filter_by(user_no=user_no,user_ps=user_ps).first()
         if user:
+            session["user_no"] = user_no
+            session["status"] = "logined"
             return "you have log in!"
         else:
             return "error no or password"
     return render_template("login.html", form=form)
 
 @accounts.route("/create")
+@verify_login
 def create():
-    pass
+    return session['status']
+
+
+@accounts.route("/logout")
+def logout():
+    session["status"] = None
+    session["user_no"] = None
+    return "you have logout!\n status: %s \n user_no: %s \n" % \
+            (session["status"], session["user_no"])
