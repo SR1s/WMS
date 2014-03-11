ba1aa9e692437442b2a20689ee7ac91a550548d5
SR1s <seemsnevermind@gmail.com>
2014-03-11 19:44:10 +0800

    build item-list page

--- WMS/views/items.py
+++ WMS/views/items.py
@@ -6,8 +6,9 @@ from WMS.views import verify_login
 
 items = Blueprint('items', __name__)
 
-@items.route("/all")
+@items.route("/list")
 @verify_login
 def all_items():
-    items = Item.query.all()
+    items = Item.query.order_by(Item.number).all()
     return str(items)
+    return render_template("item-list.html")
