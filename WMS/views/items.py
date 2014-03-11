9637c22f1c601eb42c61e7eea08d005efee592df
SR1s <seemsnevermind@gmail.com>
2014-03-11 21:21:06 +0800

    add logic to show items status

--- WMS/views/items.py
+++ WMS/views/items.py
@@ -9,6 +9,16 @@ items = Blueprint('items', __name__)
 @items.route("/list")
 @verify_login
 def all_items():
-    items = Item.query.order_by(Item.number).all()
-    return str(items)
-    return render_template("item-list.html")
+    items_raw = Item.query.order_by(Item.number.desc()).all()
+    items = dict()
+    for item_raw in items_raw:
+        if items.has_key(item_raw.number):
+            item = items.get(item_raw.number)
+            if item['info'].last_update < item_raw.last_update:
+                item['info'].last_update = item_raw.last_update
+        else:
+            item = items[item_raw.number] = dict()
+            item['info'] = item_raw
+        item[item_raw.size] = item.get(item_raw.size, 0) + item_raw.amount
+
+    return render_template("item-list.html", items=items)
