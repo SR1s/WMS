4e2dfbcf98567bdf85a5c54d050945526e5bb1ac
SR1s <seemsnevermind@gmail.com>
2014-03-15 02:57:39 +0800

    implement format data using javascript

--- WMS/static/js/wms.js
+++ WMS/static/js/wms.js
@@ -8,3 +8,51 @@ function close_message(){
     });
   }
 }
+
+function add_SML(obj){
+  var template = '<tr class="size"><td colspan="2"></td><td width="5%">XS</td><td width="5%">S</td><td width="5%">M</td><td width="5%">L</td><td width="5%">XL</td><td width="5%">XXL</td><td colspan="4"></td></tr><tr class="data"><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td><td></td><td><input type="text" /></td><td><input type="text" /></td><td><input type="text" /></td></tr><tr><td colspan="12"><button type="button" onclick="add_SML(this)">Add new Size row</button></td></tr>';
+  var parent = $(obj).parent().parent().parent();
+  var add = $(obj).parent().parent();
+  var addHTML = $(add).html()
+  $(add).remove();
+  $(parent).append(template);
+}
+
+function get_details(){
+  var data = $("#details").children();
+  var size1, size2, size3, size4, size5, size6;
+  var postdata = new Array();
+  for (var i=0;i<data.length;i++)
+  {
+    alert(data.length);
+    if($(data[i]).prop("class")=="size"){
+      size1 = $($(data[i]).children()[1]).text();
+      size2 = $($(data[i]).children()[2]).text();
+      size3 = $($(data[i]).children()[3]).text();
+      size4 = $($(data[i]).children()[4]).text();
+      size5 = $($(data[i]).children()[5]).text();
+      size6 = $($(data[i]).children()[6]).text();
+    }else if ($(data[i]).prop("class")=="data"){
+      d = new Array();
+      d["number"] = $($($(data[i]).children()[0]).children()[0]).val();
+      d["description"] = $($($(data[i]).children()[1]).children()[0]).val();
+      d["siez1"] = size1;
+      d["siez2"] = size2;
+      d["siez3"] = size3;
+      d["siez4"] = size4;
+      d["siez5"] = size5;
+      d["siez6"] = size6;
+      d['amount1'] = $($($(data[i]).children()[2]).children()[0]).val();
+      d['amount2'] = $($($(data[i]).children()[3]).children()[0]).val();
+      d['amount3'] = $($($(data[i]).children()[4]).children()[0]).val();
+      d['amount4'] = $($($(data[i]).children()[5]).children()[0]).val();
+      d['amount5'] = $($($(data[i]).children()[6]).children()[0]).val();
+      d['amount6'] = $($($(data[i]).children()[7]).children()[0]).val();
+      d["retail"] = $($($(data[i]).children()[9]).children()[0]).val();
+      d["whole"] = $($($(data[i]).children()[10]).children()[0]).val();
+      d["total"] = $($($(data[i]).children()[11]).children()[0]).val();
+      postdata.push(d);
+    }
+  }
+  console.log(postdata);
+}
