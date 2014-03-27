93ed39031f4606821ea7e4e714ecdc3a1465ea40
SR1s <seemsnevermind@gmail.com>
2014-03-27 11:38:26 +0800

    add str2datetime funciton

--- WMS/models/__init__.py
+++ WMS/models/__init__.py
@@ -11,3 +11,12 @@ from WMS.models.OrderDetail import OrderDetail
 from WMS.models.Place import Place
 from WMS.models.Reservation import Reservation
 from WMS.models.Storage import Storage
+
+from datetime import datetime
+
+def str2datetime(date_str):
+	date_p = date_str.split('/')
+	if len(date_p) == 3:
+		return datetime(int(date_p[2]),int(date_p[0]),int(date_p[1]))
+	else:
+		return None
