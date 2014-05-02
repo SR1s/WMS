5828b7ba66e647456de978ce422d77b8a7b8c9bf
SR1s <seemsnevermind@gmail.com>
2014-05-03 02:35:13 +0800

    adapt manual create order feature

--- WMS/views/order.py
+++ WMS/views/order.py
@@ -58,66 +58,39 @@ def remain():
 @order.route('/create', methods=['POST'])
 @verify_login
 def perform_create():
-    info = dict()
-    info['number'] = request.form['order_no']
-    info['date'] = str2datetime(request.form['order_date'])
-    if info['date']==None:
-        info['date']=datetime.utcnow()
-    info['items'] = json.loads(request.form['details'])
-    return _handle_create_request(info)
+    data = dict()
+    data['order_no'] = request.form['order_no']
+    data['place_id'] = session['place_id']
+    data['date'] = str2datetime(request.form['order_date'])
+    if data['date']==None:
+        data['date']=datetime.utcnow()
+    data['items'] = json.loads(request.form['details'])
+    #for item in data['items']:
+    #    item['columns'].pop('')
+    
+    if Order.query.filter_by(no=data['order_no'].upper()).first():
+        flash('Excel file format is not correct Or Order already Exist', 'error')
+        return redirect(url_for('order.list_all'))
+
+    Order.create_an_order(data)
+    flash('Success adding order')
+    return redirect(url_for('order.list_all'))
 
 @order.route('/create_by_upload', methods=['POST'])
 def create_by_upload():
     if request.files['file'].filename.split('.')[-1] not in ('xls', 'xlsx'):
         flash('Upload File illegal!')
         return redirect(url_for('index'))
+
     request.files['file'].save('temp.xls')
     data = readXls('temp.xls', 0)
+    
     if data['error'] \
        or Order.query.filter_by(no=data['order_no'].upper()).first():
         flash('Excel file format is not correct Or Order already Exist', 'error')
         return redirect(url_for('order.list_all'))
+    
     data['place_id'] = session['place_id']
     Order.create_an_order(data)
     flash('Success adding order')
-    return redirect(url_for('order.list_all'))
-
-def _handle_create_request(info):
-    if info.setdefault('status', 'normal')=='error':
-        flash('data incorrect.')
-        return redirect(url_for('order.create'))
-
-    no = info['number']
-    date = info['date']
-    details = info['items']
-
-    if Order.query.filter_by(no=no).first():
-        flash('编号%s的订单已存在' % no,'error')
-        return redirect(url_for('order.create'))
-
-    # create an new order
-    place_id = Account.query.filter_by(id=session['user_id']).first().place_id
-    order = Order(no=no, place_id=place_id,date=date)
-    db.session.add(order)
-    db.session.commit()
-    # add order details
-    for (number, d) in details.items():
-        number = d['number']
-        description = d.get('description', '')
-        # found the item or create a new item
-        item = Item.query.filter_by(number=number).first()
-        if not item:
-            item = Item(number=number,description=description)
-        # update the retail and whole price
-        item.retail = d.get('retail', 0)
-        item.whole = d.get('whole', 0)
-        db.session.add(item)
-        db.session.commit()
-
-        for column in d['columns']:
-            d = OrderDetail(item_id=item.id, order_id=order.id, \
-                            size = column['size'], \
-                            amount=column['amount'])
-            db.session.add(d)
-        db.session.commit()
     return redirect(url_for('order.list_all'))
