8455099c7329a2904c4ce0df3e5030920abdbbeb
SR1s <seemsnevermind@gmail.com>
2014-05-07 16:59:07 +0800

    add two new function

--- WMS/models/Storage.py
+++ WMS/models/Storage.py
@@ -32,6 +32,8 @@ class Storage(db.Model):
                     id=self.id,
                     size=self.size,
                     amount=self.amount,
+                    item_id=self.item_id,
+                    place_id=self.place_id,
                     number=self.item.number,
                     description=self.item.description,
                     retail=self.item.retail,
@@ -75,7 +77,38 @@ class Storage(db.Model):
         db.session.commit()
 
     @staticmethod
-    def query_all_storage():
+    def minus_batch(data):
+        '''
+        update storage bashly
+
+        @param
+            data: list
+                list of item to be update
+                data format:
+                items:list
+                    item: dict
+                        key => Value
+                        item_id => item_id
+                        place_id => place_id
+                        size => size
+                        amount => amount
+        '''
+        if data == None:
+            raise ValueError
+        for detail in data:
+            item_id = detail['item_id']
+            place_id = detail['place_id']
+            size = detail['size']
+            amount = detail['amount']
+            storage = Storage.query.filter(and_(Storage.item_id==item_id, 
+                                                Storage.place_id==place_id,
+                                                Storage.size==size)).first()
+            storage.amount = storage.amount - amount
+            db.session.add(storage)
+        db.session.commit()
+
+    @staticmethod
+    def query_all_storage(raw=False):
         '''
         @date: 14-05-04
 
@@ -94,6 +127,52 @@ class Storage(db.Model):
                                 item['amount']
         '''
         storages = Storage.query.order_by(Storage.item_id).all()
+
+        if raw:
+            return [storage.to_dict() for storage in storages]
+
+        data=dict()
+        for storage in storages:
+            storage = storage.to_dict()
+            item = data.setdefault(storage['number'], dict())
+            item.setdefault('number', storage['number'])
+            item.setdefault('description', storage['description'])
+            item.setdefault('last_update', storage['last_update'])
+            details = item.setdefault('details', dict())
+            detail = details.setdefault(storage['place'], dict())
+            detail.setdefault('place', storage['place'])
+            items = detail.setdefault('items', list())
+            items.append(dict(size=storage['size'], amount=storage['amount']))
+        for (number, storage) in data.items():
+            for (place, place_detail) in storage['details'].items():
+                items = place_detail['items']
+                place_detail['sum'] = sort_cal_all(items)
+        return data
+
+    @staticmethod
+    def query_storage(place_id=place_id, raw=False):
+        '''
+        @date: 14-05-04
+
+        @return: list of certain place storage, data structure like:
+            data: dict
+                number => storage: dict
+                    'number': String 
+                    'description': String
+                    'last_update': String
+                    'details': list
+                        place => detail: dict
+                            'place': String
+                            'sum': Integer
+                            'items': list
+                                item['size']
+                                item['amount']
+        '''
+        storages = Storage.query.filer_by(place_id=place_id).order_by(Storage.item_id).all()
+
+        if raw:
+            return [storage.to_dict() for storage in storages]
+
         data=dict()
         for storage in storages:
             storage = storage.to_dict()
