b7eb73e6235c0fb11f42d56719226bde347ba268
SR1s <seemsnevermind@gmail.com>
2014-05-12 15:24:21 +0800

    update function implement to fit new datastruct

--- WMS/models/Sell.py
+++ WMS/models/Sell.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from sqlalchemy import and_
 
 from WMS.models import db
+from WMS.models.SellDetail import SellDetail
+from WMS.models.Storage import Storage
 from WMS.views import sort_cal_all
 
 class Sell(db.Model):
@@ -27,11 +29,35 @@ class Sell(db.Model):
         @param
             data: dict
             data['place_id']: Integer
-            data['data']: datetime[Option]
-            data['details']: dict
-                number => detial:
-                          detail['size']
-                          detail['amount']
+            data['date']: datetime[Option]
+            data['items'] : dict
+                detail['item_id']: Integer
+                detail[columns]: dict
+                    key => amount: Integer
         @return sell_id if success
         '''
-        pass
+        place_id = data['place_id']
+        date = data.get('date', datetime.utcnow())
+        sell = Sell(place_id, date)
+        db.session.add(sell)
+        db.session.commit()
+
+        sell_id = sell.id
+        minus_storages = list()
+        for (number, item) in data['items'].items():
+            item_id = item['item_id']
+            for (size, amount) in item['columns'].items():
+                detail = SellDetail(item_id, sell_id, size, amount)
+                storage = dict(
+                                item_id=item_id,
+                                place_id=place_id,
+                                size=size,
+                                amount=amount,
+                          )
+                minus_storages.append(storage)
+            db.session.add(detail)
+        db.session.commit()
+
+        Storage.minus_batch(minus_storages)
+        
+        return sell_id
