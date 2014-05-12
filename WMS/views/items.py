0375c10e99ad1474eee308624d4c8091e62e55a8
SR1s <seemsnevermind@gmail.com>
2014-05-12 15:25:30 +0800

    romove useless route

--- WMS/views/items.py
+++ WMS/views/items.py
@@ -24,45 +24,6 @@ def list_all():
     info['amount'] = 0 
     return render_template("item-list.html", storages=storages, basic=basic)
 
-@items.route('/sell')
-def sell():
-    return render_template("sell-create.html")
-
-@items.route('/sell', methods=['POST'])
-def perform_create():
-    details = json.loads(request.form['details'])
-    item_result = list()
-    isOk = True
-    store = list()
-    for (key, detail) in details.items():
-        item = Item.query.filter_by(number=detail['number']).first()
-        if item:
-            place_id = session['place_id']
-            for c in detail['columns']:
-                have = Storage.query.filter(and_(Storage.item_id==item.id, \
-                                                 Storage.place_id==place_id, \
-                                                 Storage.size==c['size'])).first().amount
-                rest = have - int(c['amount'])
-                if rest<0:
-                    isOk=False
-                    flash('编号:%s 尺寸:%s的货物库存不足，剩余:%s，需要:%s' %\
-                          (detail['number'], c['size'], have, c['amount']))
-                store.append(dict(item_id=item.id, amount=rest, size=c['size']))
-        else:
-            isOk = False
-            flash('不存在编号%s的货物' % detail['number'], category='error')
-        #return json.dumps(store)
-    if isOk:
-        for s in store:
-            change = Storage.query.filter(and_(Storage.item_id==s['item_id'],\
-                                               Storage.size==s['size'], \
-                                               Storage.place_id==session['place_id'])).first()
-            change.amount=s['amount']
-            db.session.add(change)
-        db.session.commit()
-        flash('出货成功', 'normal')
-    return redirect(url_for('items.list_all'))
-
 @items.route('/transfer')
 @verify_login
 def transfer():
