8f1305563bed0d17e85a1bd42b36733129200695
SR1s <seemsnevermind@gmail.com>
2014-05-13 17:27:01 +0800

    add route to list sell-record and adjust data to fit model->se;;

--- WMS/views/sell.py
+++ WMS/views/sell.py
@@ -14,17 +14,25 @@ from WMS.views import verify_login, sort_cal_all
 
 sell = Blueprint('sell', __name__)
 
+@sell.route('/list')
+@verify_login
+def list_all():
+    sells = [ sell.to_dict() for sell in Sell.query.all() ]
+    return render_template('sell-list.html', sells=sells)
+
 @sell.route('/create')
 @verify_login
 def create():
     return render_template("sell-create.html")
 
 @sell.route('/create', methods=['POST'])
+@verify_login
 def perform_create():
     place_id = session['place_id']
+    account_id = session['user_id']
     date = request.form.get('date', datetime.utcnow())
     details = json.loads(request.form['details'])
-
+    return Log.json(details)
     valid_data=dict()
     # validation Start-->
     is_data_valid = True
@@ -34,7 +42,7 @@ def perform_create():
             flash('Item %s not exist' % number, 'error')
             is_data_valid = False
             continue
-        item_id = item.id
+        retail = detail['retail']
         for (size, amount) in detail['columns'].items():
             stor = Storage.query.filter(and_(Storage.place_id== place_id,
                                              Storage.item_id == item_id,
@@ -42,6 +50,7 @@ def perform_create():
             if stor and stor.amount >= amount:
                 valid_detail = valid_data.setdefault(number, dict())
                 valid_detail.setdefault('item_id', item_id)
+                valid_detail.setdefault('retail', item_id)
                 columns = valid_detail.setdefault('columns', dict())
                 columns[size] = amount
             else:
@@ -58,6 +67,7 @@ def perform_create():
     # assembling data
     data = dict()
     data['place_id'] = place_id
+    data['account_id'] = account_id
     data['date'] = date
     data['items'] = valid_data
     Sell.create_sell_record(data)
